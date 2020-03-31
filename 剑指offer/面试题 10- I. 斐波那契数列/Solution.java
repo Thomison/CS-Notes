31f02e5b80ae044e6c04fbc1cd3b7b4ccf4f01a1
Thomison <62683644+Thomison@users.noreply.github.com>
2020-03-31 16:12:26 +0800

    Create Solution.java

---
+++ 剑指offer/面试题 10- I. 斐波那契数列/Solution.java
@@ -0,0 +1,27 @@
+/*
+写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项。斐波那契数列的定义如下：
+
+F(0) = 0, F(1) = 1
+F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1
+*/
+class Solution {
+    final int mod = 1000000007;
+    
+    // return f(n) : f(n) = f(n-1) + f(n-2)
+    
+    public int fib(int n) {
+        int[] dp = new int[101];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % mod;
+        }
+        
+        return dp[n];
+    }
+    
+}
